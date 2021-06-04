74a5caca2641a2ed9093977dd0b15e30fb061ce2
Md Aasim Ahmad <aasimahmad5@gmail.com>
2021-06-04 21:39:15 +0530

    Added styling and search functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState, useRef } from 'react';
 
 function App() {
+  const [ingredientList, updateIngredientList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const inputRef = useRef(null);
+  const API_KEY = 'd186145028ed01ef715a6387294dd388';
+  const APP_ID = '7078ebfb';
+
+  const search = () => {
+    searchForRecipe(inputRef.current.value);
+    inputRef.current.value = '';
+  };
+  const searchForRecipe = (query) => {
+    setLoading(true);
+    let url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${API_KEY}`;
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((res) => {
+        updateIngredientList(res.hits);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  };
+
+  const keyHandler = (e) => {
+    if (e.key === 'Enter') {
+      search();
+    }
+  };
+
+  useEffect(() => {
+    searchForRecipe('paneer');
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Title">RECIPE FINDER</div>
+        <div className="InputWrapper">
+          <input
+            ref={inputRef}
+            placeholder="Search for Recipe"
+            onKeyDown={(e) => keyHandler(e)}
+          />
+          <button onClick={search}>Search</button>
+        </div>
+
+        {loading && <p>Loading...</p>}
+
+        <div className="Wrapper">
+          {ingredientList.map(({ recipe }) => {
+            const { label, image, ingredientLines } = recipe;
+            return (
+              <div key={label} className="Ingredient">
+                <span>{label}</span>
+                <img src={image} alt="Recipe" />
+                <div className="Steps">
+                  {ingredientLines.map((step, index) => {
+                    return <p key={index}> {step} </p>;
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </header>
     </div>
   );
